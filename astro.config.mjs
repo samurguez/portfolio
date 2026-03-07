4e50a6958a7424019714c3b3927924d1d72361a3
samurguez <samurguez@Samu-MacBook-Pro.local>
2026-03-07 11:34:43 +0000

    fix: desactivar sitemap temporalmente

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
+// import sitemap from "@astrojs/sitemap"; // Temporarily disabled
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://www.samuelrodriguezdesign.com",
-	integrations: [tailwind(), sitemap()],
+	integrations: [tailwind()],
 });
